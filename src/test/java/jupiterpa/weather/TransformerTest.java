5ecc8244b46881223779603704e5fd8a92784f8f
D043312 <D043312@WDFN33414359A.emea.global.corp.sap>
2018-01-31 11:22:37 +0100

    Unit Tests Working again

--- src/test/java/jupiterpa/weather/TransformerTest.java
+++ src/test/java/jupiterpa/weather/TransformerTest.java
@@ -1,16 +1,11 @@
 package jupiterpa.weather;
 
-import java.util.Collection;
-import java.util.Date;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import jupiterpa.weather.domain.model.*;
 import jupiterpa.weather.domain.service.*;
 
@@ -23,7 +18,7 @@ public class TransformerTest {
     	TemplateEntity entity = new TemplateEntity("Test");
 
     	TemplateEntity newEntity = TemplateTransformer.transform(entity);
-    	
-    	assertEquals(newEntity.toString(), "Test" );
+   	
+    	assertEquals("TestT",newEntity.toString());
     }
 }
